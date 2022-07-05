1eb5c3957bfa36213fd9c604103dcfafe3176961
Jution Candra Kirana <jutionck@gmail.com>
2022-07-05 09:02:28 +0700

    Init Project

--- category_controller.go
+++ category_controller.go
@@ -25,11 +25,20 @@ func (cc *CategoryController) createNewCategory(c *gin.Context) {
 	}
 }
 
+func (cc *CategoryController) getAllCategory(c *gin.Context) {
+	category := cc.ctUseCase.List()
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+		"data":    category,
+	})
+}
+
 func NewCategoryController(router *gin.Engine, ctUseCase CategoryUseCase) *CategoryController {
 	ctController := CategoryController{
 		router:    router,
 		ctUseCase: ctUseCase,
 	}
 	router.POST("/category", ctController.createNewCategory)
+	router.GET("/category", ctController.getAllCategory)
 	return &ctController
 }
